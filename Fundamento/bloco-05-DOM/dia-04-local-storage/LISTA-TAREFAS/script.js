059076d8f78e84d9dbb63307fc4cb4c5693954a4
luansa1995 <luan.augusto.sa@gmail.com>
2022-02-23 22:31:57 -0300

    iniciando js 100%

---
+++ Fundamento/bloco-05-DOM/dia-04-local-storage/LISTA-TAREFAS/script.js
@@ -0,0 +1,10 @@
+let whereUl = document.querySelector("ul");
+let whereButton = document.querySelector("button");
+
+whereButton.addEventListener("click", addTarefa)
+
+function addTarefa () {
+    
+}
+
+
